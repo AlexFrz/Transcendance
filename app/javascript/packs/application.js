c93a6c8176bd783d7486c05d66774b214957d479
Alex Frezoul <frezoul.alex@gmail.com>
2020-07-10 16:08:52 +0200

    hamburger menu done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//=require custom
 
 require("@rails/ujs").start();
 require("turbolinks").start();
@@ -9,6 +10,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("jquery");
 require("jquery_ujs");
+require("custom")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,3 +26,4 @@ import "regenerator-runtime/runtime";
 import {} from "jquery-ujs";
 import "@fontawesome/fontawesome-free/js/all";;
 /
+
