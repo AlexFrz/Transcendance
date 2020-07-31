160d6e00305e4d7ac39107defccf95a412bd251e
Alex Frezoul <frezoul.alex@gmail.com>
2020-07-31 13:55:00 +0200

    devise fix: suppress login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
-
-  attr_writer :login
+         :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
 
@@ -26,12 +24,4 @@ class User < ApplicationRecord
     end
   end
   
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
-  end
 end
